dcfbc8d40a7ccfdd28d934ac57c4b422945f4716
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2024-01-28 14:20:01 +0700

    feat: add Paper

---
+++ Paper.hpp
@@ -0,0 +1,27 @@
+#ifndef FOO_HPP
+#define FOO_HPP
+
+#include <iostream>
+
+class Paper {
+    private: 
+        // attribute private
+        char name;
+        int n_fold;
+    
+    public:
+        // ctor
+        Paper(char c);
+        // dtor
+        ~Paper();
+
+        // attribute public
+        int number;
+
+        // method public
+        void fold();
+        void glue();
+        void setName(char c);
+};
+
+#endif
